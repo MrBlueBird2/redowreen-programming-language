f344a8fc0438b1996fa979361473e6eeb2b5e1f8
Badri Vishal <bv7887393@gmail.com>
2022-02-11 22:09:17 +0530

    Hehe Nice Fix

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ while True:
 	text = input('Comthon > ')
 	l = []
 	if "+" in text:
-		text = text.replace("+", "plus")
-		splitted_text = text.split()
+		text = text.replace("+", " ")
+		text = text.split()
 		print(text)
