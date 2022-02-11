60388dfe02046fbeb1d5676816a9dc2e774afb81
Badri Vishal <bv7887393@gmail.com>
2022-02-11 22:40:25 +0530

    Added Multiplication

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ while True:
 				i = int(i)
 				ans += i
 		print(ans)
+	elif "*" in text:
+		ans = 1
+		text = text.replace("*", " ")
+		text = text.split()
+		for i in text:
+			if "." in i:
+				i = float(i)
+				ans *= i
+			else:
+				i = int(i)
+				ans *= i
+		print(ans)
