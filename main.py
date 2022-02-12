db2d00422df9eb8c4aaa70f1f73a99de321f0627
Badri Vishal <bv7887393@gmail.com>
2022-02-12 18:52:45 +0530

    Added Subtraction Feature

--- main.py
+++ main.py
@@ -26,3 +26,13 @@ while True:
 				i = int(i)
 				ans *= i
 		print(ans)
+	elif '-' in text:
+		ans = 0
+		text = text.replace("-", " ")
+		text = text.split()
+		default = int(text[0])
+		for i in range(1, len(text)):
+			answer = default - int(text[i])
+			default = int(text[i])
+			ans += answer
+		print(ans)
